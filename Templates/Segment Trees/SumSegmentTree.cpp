a34d24bb798cb9e357fe8f49f650b876389ea254
Deyuan Li <darrellli234@gmail.com>
2018-08-31 16:28:03 -0400

    Update SumSegmentTree.cpp

--- Templates/Segment Trees/SumSegmentTree.cpp
+++ Templates/Segment Trees/SumSegmentTree.cpp
@@ -1,22 +1,22 @@
 struct SumSegmentTree {
 	int sz;
-	int *tree;
+	ll *tree;
 
 	SumSegmentTree(int s) {
 		sz = 1 << (int)ceil(log2(s));
 		tree = new int[2 * sz];
-		memset(tree, 0, 2 * sz * sizeof(int));
+		memset(tree, 0, 2 * sz * sizeof(ll));
 	}
-	void add(int ind, int x) {
+	void add(int ind, ll x) {
 		ind += sz;
 		tree[ind] += x;
 		for (ind /= 2; ind >= 1; ind /= 2) {
 			tree[ind] = tree[2 * ind] + tree[2 * ind + 1];
 		}
 	}
-	int sum(int a, int b) { //sums [a,b] inclusive
+	ll sum(int a, int b) { //sums [a,b] inclusive
 		a += sz; b += sz;
-		int s = 0;
+		ll s = 0;
 		while (a <= b) {
 			if (a % 2 == 1) s += tree[a++];
 			if (b % 2 == 0) s += tree[b--];
