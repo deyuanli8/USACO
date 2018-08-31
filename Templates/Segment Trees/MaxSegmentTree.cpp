16b01d749e78e2a3c227f4a3cd16ed29e1d1597e
Deyuan Li <darrellli234@gmail.com>
2018-08-31 16:25:47 -0400

    Update MaxSegmentTree.cpp

--- Templates/Segment Trees/MaxSegmentTree.cpp
+++ Templates/Segment Trees/MaxSegmentTree.cpp
@@ -1,22 +1,22 @@
 struct MaxSegmentTree {
 	int sz;
-	int *tree;
+	ll *tree;
 
 	MaxSegmentTree(int s) {
 		sz = 1 << (int)ceil(log2(s));
-		tree = new int[2 * sz];
-		memset(tree, 0, 2 * sz * sizeof(int));
+		tree = new ll[2 * sz];
+		memset(tree, 0, 2 * sz * sizeof(ll));
 	}
-	void add(int ind, int x) {
+	void add(int ind, ll x) {
 		ind += sz;
 		tree[ind] += x;
 		for (ind /= 2; ind >= 1; ind /= 2) {
 			tree[ind] = max(tree[2 * ind], tree[2 * ind + 1]);
 		}
 	}
-	int sum(int a, int b) { //max of [a,b] inclusive
+	ll sum(int a, int b) { //max of [a,b] inclusive
 		a += sz; b += sz;
-		int s = tree[a];
+		ll s = tree[a];
 		while (a <= b) {
 			if (a % 2 == 1) s = max(s, tree[a++]);
 			if (b % 2 == 0) s = max(s, tree[b--]);
