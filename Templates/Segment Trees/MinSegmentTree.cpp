8c0710982370d66e713d751f67b23fb10ac0bd1d
Deyuan Li <darrellli234@gmail.com>
2018-10-08 18:48:51 -0400

    Update MinSegmentTree.cpp

--- Templates/Segment Trees/MinSegmentTree.cpp
+++ Templates/Segment Trees/MinSegmentTree.cpp
@@ -7,6 +7,17 @@ struct MinSegmentTree {
 		tree = new ll[2 * sz];
 		memset(tree, 0, 2 * sz * sizeof(ll));
 	}
+	MinSegmentTree(int s, ll* st) {
+		sz = 1 << (int)ceil(log2(s));
+		tree = new ll[2 * sz];
+		memset(tree, 0, 2 * sz * sizeof(ll));
+		FOR(i, s) {
+			tree[i + sz] = st[i];
+		}
+		F0Rd(ind, 1, sz) {
+			tree[ind] = min(tree[2 * ind], tree[2 * ind + 1]);
+		}
+	}
 	void add(int ind, ll x) {
 		ind += sz;
 		tree[ind] += x;
@@ -14,7 +25,7 @@ struct MinSegmentTree {
 			tree[ind] = min(tree[2 * ind], tree[2 * ind + 1]);
 		}
 	}
-	ll sum(int a, int b) { //min of [a,b] inclusive
+	ll query(int a, int b) { //min of [a,b] inclusive
 		a += sz; b += sz;
 		ll s = tree[a];
 		while (a <= b) {
