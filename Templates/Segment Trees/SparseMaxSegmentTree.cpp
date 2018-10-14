25c77f0d7457e13da0ff93e1e6dff3b193c7c8eb
Deyuan Li <darrellli234@gmail.com>
2018-10-14 12:11:48 -0400

    Update SparseMaxSegmentTree.cpp

--- Templates/Segment Trees/SparseMaxSegmentTree.cpp
+++ Templates/Segment Trees/SparseMaxSegmentTree.cpp
@@ -1,4 +1,4 @@
-const int SZ = 1 << 25;
+const int SZ = 1 << 20;
 struct node {
 	ll val;
 	node* c[2];
@@ -25,10 +25,23 @@ struct node {
 		}
 		val = max((c[0] ? c[0]->val : 0), (c[1] ? c[1]->val : 0));
 	}
+
 	ll query(int ql, int qr, int l = 0, int r = SZ - 1) {
 		if (ql <= l && r <= qr) return val;
 		if (r < ql || l > qr) return 0;
 		int mid = (l + r) / 2;
 		return max((c[0] ? c[0]->query(ql, qr, l, mid) : 0), (c[1] ? c[1]->query(ql, qr, mid + 1, r) : 0));
+	}
+	void upd2d(int ind, node* c1, node* c2, int l = 0, int r = SZ - 1) {
+		if (l != r) {
+			int mid = (l + r) / 2;
+			if (ind <= mid) {
+				getc(0)->upd2d(ind, c1 ? c1->c[0] : NULL, c2 ? c2->c[0] : NULL, l, mid);
+			}
+			else {
+				getc(1)->upd2d(ind, c1 ? c1->c[1] : NULL, c2 ? c2->c[1] : NULL, mid + 1, r);
+			}
+		}
+		val = max(c1->val, c2->val);
 	}
 };
