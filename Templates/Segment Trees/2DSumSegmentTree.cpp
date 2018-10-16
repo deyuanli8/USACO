0eb3cfe8f01fd4df98ce195618bd927264b627db
Deyuan Li <darrellli234@gmail.com>
2018-10-16 18:01:01 -0400

    Update 2DSumSegmentTree.cpp

--- Templates/Segment Trees/2DSumSegmentTree.cpp
+++ Templates/Segment Trees/2DSumSegmentTree.cpp
@@ -35,7 +35,7 @@ struct SumSegmentTree2D {
 			tree[ind1].add(ind2, tree[2 * ind1].tree[ind2 + tree[ind1].sz] + tree[2 * ind1 + 1].tree[ind2 + tree[ind1].sz] - tree[ind1].tree[tree[ind1].sz + ind2]);
 		}
 	}
-	ll query(int a, int b, int c, int d) { //min of rows [a,b], columns [c,d] inclusive
+	ll query(int a, int b, int c, int d) { //sum of rows [a,b], columns [c,d] inclusive
 		a += sz; b += sz;
 		ll s = 0;
 		while (a <= b) {
