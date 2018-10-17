a0d7e384ff562ab1cc383b908eb67a7f7e2436ed
Deyuan Li <darrellli234@gmail.com>
2018-10-17 14:09:50 -0400

    Update 2DSparseMaxSegmentTree.cpp

--- Templates/Segment Trees/2DSparseMaxSegmentTree.cpp
+++ Templates/Segment Trees/2DSparseMaxSegmentTree.cpp
@@ -1,11 +1,15 @@
 struct maxnode {
 	node segment;
 	maxnode* c[2];
-	
+
 	maxnode() {
 		segment = node();
 		c[0] = NULL;
 		c[1] = NULL;
+	}
+	bool overlap(int w, int x, int y, int z) {
+		if (z < w || y > x) return false;
+		return true;
 	}
 	void update(int x, int y, ll v, int l = 0, int r = SZ - 1) {
 		if (l == x && r == x) {
@@ -25,8 +29,8 @@ struct maxnode {
 	}
 	ll query(int w, int x, int y, int z, int l = 0, int r = SZ - 1) {
 		if (w <= l && r <= x) return segment.query(y, z);
-		if (x < l || w > r) return 0;
+		if (x < l || w > r) return -INF;
 		int mid = (l + r) / 2;
-		return max((c[0] ? c[0]->query(w, x, y, z, l, mid) : 0), (c[1] ? c[1]->query(w, x, y, z, mid + 1, r) : 0));
+		return max((c[0] ? c[0]->query(w, x, y, z, l, mid) : (overlap(w, x, l, mid) ? 0 : -INF)), (c[1] ? c[1]->query(w, x, y, z, mid + 1, r) : (overlap(w, x, mid + 1, r) ? 0 : -INF)));
 	}
 };
